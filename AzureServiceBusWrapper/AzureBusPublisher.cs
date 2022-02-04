0d75c09c50bce2294d82b60e0f6392c3880a36c7
pedro <pedro@RPC-MXL40623L3>
2022-02-04 11:42:29 -0500

    first commit

---
+++ AzureServiceBusWrapper/AzureBusPublisher.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusWrapper
+{
+    public static class AzureBusPublisher
+    {
+        
+        public static Task Publish<TBody>(TBody body,string connectionString,string queueName)
+        {
+            return Task.Run(async () =>
+            {
+                ServiceBusClient client = default;
+                ServiceBusSender sender = default;
+                try
+                {
+                    client = new ServiceBusClient(connectionString);
+                    sender = client.CreateSender(queueName);
+                    string serializedBody = JsonConvert.SerializeObject(body);
+                    ServiceBusMessage message = new ServiceBusMessage(Encoding.UTF8.GetBytes(serializedBody));
+                    await sender.SendMessageAsync(message);
+                }
+                finally
+                {
+                    await sender.DisposeAsync();
+                    await client.DisposeAsync();
+                }
+            });
+        }
+    }
+}
---
+++ AzureServiceBusWrapper/AzureBusSusbcriber.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusWrapper
+{
+    public static class AzureBusSusbcriber
+    {
+        public static async Task Susbscribe(string connectionString,string queueName, Func<ProcessMessageEventArgs, Task> onSuccess, Func<ProcessErrorEventArgs, Task> onError)
+        {
+
+            ServiceBusClient client = new ServiceBusClient(connectionString);
+            ServiceBusProcessor processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+            try
+            {
+                // add handler to process messages
+                processor.ProcessMessageAsync += onSuccess;
+
+                // add handler to process any errors
+                processor.ProcessErrorAsync += onError;
+
+                // start processing 
+                await processor.StartProcessingAsync();
+
+                // stop processing 
+                await processor.StopProcessingAsync();
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
